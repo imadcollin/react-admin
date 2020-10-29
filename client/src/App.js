c0e48b609ac1ebb0cfa0bb972a6a60c02fb9d9f5
imadcollin <imad.collin@gmail.com>
2020-10-29 09:40:17 +0100

    Refactoring post componant

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import {Admin , Resource } from 'react-admin';
 import restProvider from 'ra-data-simple-rest'; 
 
-import PostList from './Components/Posts/posts' 
-import PostCreate from "./Components/Posts/createPost"
-import EditPost from "./Components/Posts/editPost"
-import PostShow from "./Components/Posts/showPost"
-
+import * as posts from "./Components/Posts/posts"
 import * as user from "./Components/Users/users"
 import * as comments from "./Components/Comments/comments"
+
 import authProvider from "./Services/authProvider"
 
 function App() {
@@ -16,7 +13,7 @@ function App() {
       
           <Admin authProvider={authProvider} dataProvider={restProvider('http://localhost:5000')}>
             
-        <Resource name="posts" list={PostList}  create={PostCreate} edit={EditPost} show={PostShow}/>
+        <Resource name="posts" list={posts.PostList}  create={posts.PostCreate} edit={posts.PostEdit} show={posts.PostShow}/>
         <Resource name="users" list={user.UserList} create={user.CreateUser} edit={user.UserEdit} show={user.ShowUsers}  />
         <Resource name="comments" list={comments.CommentList} create={comments.CreateComment} edit={comments.EditComment} show={comments.ShowComment}  />
 
--- client/src/Components/Posts/posts.js
+++ client/src/Components/Posts/posts.js
@@ -7,22 +7,69 @@ import {
   DeleteButton,
   ReferenceInput,
   SelectInput,
-  ReferenceField
+  ReferenceField,
+  Show,
+  SimpleShowLayout,
+  RichTextField,
+  Edit,
+  SimpleForm,
+  TextInput,
+  Create,
 } from "react-admin";
 
 const PostList = (props) => {
-	return <List {...props}>
-    <Datagrid>
- 
-    <TextField source="id" />
-            <ReferenceField label="User" source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-      <TextField source="title" />
-      <TextField source="body" />
-      <EditButton basePath="/posts" />
-      <DeleteButton basePath="/posts" />
-    </Datagrid>
-  </List>
+  return (
+    <List {...props}>
+      <Datagrid>
+        <TextField source="id" />
+        <ReferenceField label="User" source="userId" reference="users">
+          <TextField source="name" />
+        </ReferenceField>
+        <TextField source="title" />
+        <TextField source="body" />
+        <EditButton basePath="/posts" />
+        <DeleteButton basePath="/posts" />
+      </Datagrid>
+    </List>
+  );
 };
-export default PostList; 
+
+const PostCreate = (props) => {
+  return (
+    <Create title="Create a Post" {...props}>
+      <SimpleForm>
+        <ReferenceInput label="User" source="userId" reference="users">
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+        <TextInput source="title"></TextInput>
+        <TextInput multiline source="body"></TextInput>
+      </SimpleForm>
+    </Create>
+  );
+};
+
+const PostEdit = (props) => {
+  return (
+    <Edit title="Create a Post" {...props}>
+      <SimpleForm>
+        <ReferenceInput label="User" source="userId" reference="users">
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+        <TextInput source="title"></TextInput>
+        <TextInput multiline source="body"></TextInput>
+      </SimpleForm>
+    </Edit>
+  );
+};
+
+const PostShow = (props) => {
+  return (
+    <Show title="Show a Post" {...props}>
+      <SimpleShowLayout>
+        <TextField source="title" />
+        <RichTextField source="body" />
+      </SimpleShowLayout>
+    </Show>
+  );
+};
+export { PostList, PostShow, PostEdit, PostCreate };
