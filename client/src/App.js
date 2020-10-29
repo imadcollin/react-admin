0a50b58f3e3fba29ed98183f8daff1e2e6becdb5
imadcollin <imad.collin@gmail.com>
2020-10-29 14:10:12 +0100

    Replace simple json with ServerProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import {Admin , Resource } from 'react-admin';
-import restProvider from 'ra-data-simple-rest'; 
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-import * as posts from "./Components/Posts/posts"
-import * as user from "./Components/Users/users"
-import * as comments from "./Components/Comments/comments"
+import * as posts from "./Components/Posts/posts";
+import * as user from "./Components/Users/users";
+import * as comments from "./Components/Comments/comments";
 
-import authProvider from "./Services/authProvider"
-import dashboard from "./Components/Dashboard/dashboard"
+import authProvider from "./Services/authProvider";
+import dashboard from "./Components/Dashboard/dashboard";
 function App() {
   return (
     <div className="App">
-      
-          <Admin dashboard={dashboard} authProvider={authProvider} dataProvider={restProvider('http://localhost:5000')}>
-            
-        <Resource name="posts" list={posts.PostList}  create={posts.PostCreate} edit={posts.PostEdit} show={posts.PostShow}/>
-        <Resource name="users" list={user.UserList} create={user.CreateUser} edit={user.UserEdit} show={user.ShowUsers}  />
-        <Resource name="comments" list={comments.CommentList} create={comments.CreateComment} edit={comments.EditComment} show={comments.ShowComment}  />
-
-    </Admin>,
+      <Admin
+        dashboard={dashboard}
+        authProvider={authProvider}
+        dataProvider={jsonServerProvider("http://localhost:5000")}
+      >
+        <Resource
+          name="posts"
+          list={posts.PostList}
+          create={posts.PostCreate}
+          edit={posts.PostEdit}
+          show={posts.PostShow}
+        />
+        <Resource
+          name="users"
+          list={user.UserList}
+          create={user.CreateUser}
+          edit={user.UserEdit}
+          show={user.ShowUsers}
+        />
+        <Resource
+          name="comments"
+          list={comments.CommentList}
+          create={comments.CreateComment}
+          edit={comments.EditComment}
+          show={comments.ShowComment}
+        />
+      </Admin>
+      ,
     </div>
   );
 }
