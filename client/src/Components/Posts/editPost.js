923223ddcf0e98fb1a0d4942ec324ed15aee8721
imadcollin <imad.collin@gmail.com>
2020-10-29 09:26:24 +0100

    Connect posts to users

--- client/src/Components/Posts/editPost.js
+++ client/src/Components/Posts/editPost.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import {Edit, SimpleForm, TextInput } from 'react-admin'
+import {Edit, SimpleForm, TextInput, SelectInput, ReferenceInput } from 'react-admin'
 const PostEdit=(props)=> {
 	return (
 		<Edit title='Create a Post' {...props}>
 
 		<SimpleForm>
+		<ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
 			<TextInput source='title'></TextInput> 
 			<TextInput multiline source='body'></TextInput> 
 		</SimpleForm>
--- client/src/Components/Posts/posts.js
+++ client/src/Components/Posts/posts.js
@@ -5,12 +5,19 @@ import {
   TextField,
   EditButton,
   DeleteButton,
+  ReferenceInput,
+  SelectInput,
+  ReferenceField
 } from "react-admin";
 
 const PostList = (props) => {
 	return <List {...props}>
     <Datagrid>
-      <TextField source="id" />
+ 
+    <TextField source="id" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
       <TextField source="title" />
       <TextField source="body" />
       <EditButton basePath="/posts" />
